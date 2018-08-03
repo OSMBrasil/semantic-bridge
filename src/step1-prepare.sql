4aa63249cdfdfe06cd9987cb8a42ff8d91c223e1
User <ppkrauss@gmail.com>
2018-08-02 21:27:08 -0300

    add sql to load datasets

---
+++ src/step1-prepare.sql
@@ -0,0 +1,44 @@
+/**
+ * STEP 1 - SQL PREPARATION
+ */
+
+--
+-- PREPARE WITH
+-- cd /tmp
+-- git clone https://github.com/OSMBrasil/semantic-bridge.git
+--
+
+CREATE EXTENSION file_fdw;
+CREATE SERVER files FOREIGN DATA WRAPPER file_fdw;
+
+DROP SCHEMA IF EXISTS tmp1 CASCADE;
+CREATE SCHEMA tmp1;
+
+CREATE FOREIGN TABLE tmp1.wd_br_raw (
+  wd_id int,           osm_relid int,
+  latlong_wgs84 text,  ibge_id int,
+  name text
+) SERVER files OPTIONS (
+  filename '/tmp/semantic-bridge/data/dumps_wd/BR_items.csv',
+  format 'csv',
+  header 'true'
+);
+
+CREATE FOREIGN TABLE tmp1.osm_br_raw (
+  osm_type text,       osm_id int,
+  wd_id text,          name text
+) SERVER files OPTIONS (
+  filename '/tmp/semantic-bridge/data/dumps_osm/BR_elements.csv',
+  format 'csv',
+  header 'true'
+);
+
+CREATE VIEW tmp1.wd_osm_join1 AS
+  SELECT o.wd_id, osm_type, o.osm_id,
+         CASE WHEN osm_type='relation' AND w.osm_relid=osm_id THEN true ELSE false END as osm_was_matching,
+         '('|| w.name ||' | '|| o.name||')' as names
+  FROM tmp1.wd_br_raw w INNER JOIN tmp1.osm_br_raw o
+    ON o.wd_id::text = substr(w.wd_id,1)
+;
+
+-- falta carregar cidades e estados para descontar.
